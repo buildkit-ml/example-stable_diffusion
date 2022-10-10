f6b9ac0cf76d617f32fff7a2e5c96db2ac3718dd
Xiaozhe Yao <askxzyao@gmail.com>
2022-10-10 15:31:10 +0000

    add seed

--- app/stable_diffusion.py
+++ app/stable_diffusion.py
@@ -1,25 +1,27 @@
+import json
 import os
 import sys
 import time
 from random import randint
 from typing import Dict
+import matplotlib.pyplot as plt
 import requests
-from diffusers import StableDiffusionPipeline
-import torch
 
 sys.path.append("./")
-from common import FastInferenceInterface
-
+from common.fast_inference import FastInferenceInterface
+from diffusers import StableDiffusionPipeline
+import torch
 
 class FastStableDiffusion(FastInferenceInterface):
     def __init__(self, model_name: str, args=None) -> None:
         super().__init__(model_name, args)
         self.pipe = StableDiffusionPipeline.from_pretrained(
-            "/model/snapshots/a304b1ab1b59dd6c3ba9c40705c29c6de4144096/",
+            "CompVis/stable-diffusion-v1-4",
             revision="fp16", 
             torch_dtype=torch.float16,
         )
         self.pipe = self.pipe.to("cuda")
+        self.generator = torch.Generator(device="cuda")
 
     def infer(self, job_id, args) -> Dict:
         coord_url = os.environ.get("COORDINATOR_URL", "localhost:8092/my_coord")
@@ -30,18 +32,31 @@ class FastStableDiffusion(FastInferenceInterface):
                 "status": "running",
             }
         )
+        
+        self.generator.manual_seed(args['seed'])
+
+        latents = torch.randn(
+            (1, self.pipe.unet.in_channels, 512 // 8, 512 // 8),
+            generator = self.generator,
+            device = "cuda"
+        )
         start = time.time()
-        image = self.pipe(args['input']).images[0]
+        with torch.autocast("cuda"):
+            image = self.pipe(
+                [args['input']] * 1,
+                guidance_scale=7.5,
+                latents = latents,
+            )["sample"][0]
         end = time.time()
         # save images to file
         fileid = randint(0, 100000)
-        image.save(f"/app/results/image_{fileid}.png")
+        image.save(f"image_{fileid}.png")
         # upload images to s3
-        with open(f"/app/results/image_{fileid}.png", "rb") as fp:
+        with open(f"image_{fileid}.png", "rb") as fp:
             files = {"file": fp}
             res = requests.post("https://planetd.shift.ml/file", files=files).json()
             filename=res["filename"]
-        os.remove(f"/app/results/image_{fileid}.png")
+        os.remove(f"image_{fileid}.png")
         # delete the file
         print("sending requests to global")
         # write results back
@@ -57,3 +72,8 @@ class FastStableDiffusion(FastInferenceInterface):
             },
         )
         return {"output": filename}
+
+
+if __name__ == "__main__":
+    fip = FastStableDiffusion(model_name="together.StableDiffusion")
+    fip.start()
